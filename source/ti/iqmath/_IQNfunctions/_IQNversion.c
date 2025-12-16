96d6232b8b822d0b7b39a9883e3fb41555d062d6
Yuna Celisse <29951517@qq.com>
2025-12-16 13:40:01 +0800

    仅保留keil文件夹

--- source/ti/iqmath/_IQNfunctions/_IQNversion.c
+++
@@ -1,6 +0,0 @@
-const char IQmathLibVersionString[] = "IQmathLib version 02_00_00_00";
-
-const char *_IQmathLibVersionString(void)
-{
-    return IQmathLibVersionString;
-}
--- tools/bsl/dfu_host_utility/source_code/dfu_host_utility_exe_update.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo "---- Building DFU host utility exe -----------------------------"
-
-gcc hostutility.c -o ../hostutility.exe
-gcc hostutility_interface.c -o ../hostutility_interface.exe
-pause
